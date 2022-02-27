adf98ab2fd853847ac27a24109b8bb548dc87569
Nilesh Indore <indorenilesh@gmail.com>
2022-02-27 12:23:32 +0530

    update

---
+++ if-con.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Enter any number : "
+read num1
+
+if [ $num1 -lt 10 ]
+then
+    echo "This is single digit number."
+elif [[ $num1 -gt 9 && $num1 -lt 100 ]]
+then
+    echo "This is double digit number"
+elif [[ $num1 -gt 99 && $num1 -lt 1000 ]]
+then
+    echo "This is three digit number" 
+else
+    echo "This number is more than 3 digit."
+fi 
